4f51c03997144b1435fd5fc611aac6cf3a790740
Sabana Ghale <thomasvl@stud.ntnu.no>
2021-05-02 20:41:39 +0100

    Implemented structures for mealplanner service

--- utils/struct.go
+++ utils/struct.go
@@ -1,2 +1,18 @@
 package utils
 
+type Fridge struct {
+	Ingredients []string
+}
+
+//Recipe struct holds all data from the spoonacular API used to find recipes
+type Recipe struct {
+	Name                 string           `json:"title"`
+	Image                string           `json:"image"`
+	MissedIngredients    []IngredientList `json:"missedIngredients"`   //Missing ingredients from
+	UsedIngredientsCount int              `json:"usedIngredientCount"` //Amounts of Ingredients used
+	UsedIngredients      []IngredientList `json:"usedIngredients"`
+}
+
+type IngredientList struct {
+	IngredientName string `json:"name"`
+}
