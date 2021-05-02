c0f62c480da74fea79bbe25fdac9f2f3af5441ed
Voxkyrie <haavardostli@gmail.com>
2021-05-02 13:44:00 +0200

    Mutli-page embed prototype

--- bot/bot.go
+++ bot/bot.go
@@ -6,11 +6,12 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
+	ds "github.com/bwmarrin/discordgo"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 )
 
 /*
@@ -18,18 +19,20 @@ import (
 */
 var (
 	Token string
-	BotPrefix = "@news"
+	BotPrefix = "@test"
 	FlagPrefix = "-"
 )
 
 func init() {
 	Token = os.Getenv("BOT_TOKEN")
-	flag.StringVar(&Token, "t", "", "Bot Token")
+	if Token == "" {
+		flag.StringVar(&Token, "t", "", "Bot Token")
+	}
 	flag.Parse()
 }
 
 func main(){
-	discord, err := discordgo.New("Bot " + Token)
+	discord, err := ds.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
@@ -38,7 +41,7 @@ func main(){
 	// Register the messageCreate func as a callback for MessageCreate events.
 	discord.AddHandler(router)
 	// In this example, we only care about receiving message events.
-	discord.Identify.Intents = discordgo.IntentsGuildMessages
+	discord.Identify.Intents = ds.IntentsGuildMessages
 
 
 	// Open a websocket connection to Discord and begin listening.
@@ -60,8 +63,8 @@ func main(){
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
-func router(s *discordgo.Session, m *discordgo.MessageCreate) {
-	var reply = discordgo.MessageEmbed{}
+func router(s *ds.Session, m *ds.MessageCreate) {
+	var reply = ds.MessageEmbed{}
 	var err error
 
 	// Ignore all messages created by the bot itself,
@@ -102,9 +105,64 @@ func router(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Send reply
 	if err != nil {			// Error handling
 		s.ChannelMessageSend(m.ChannelID, err.Error())
+	} else if true { //If reply is an array of embeds, true for testing
+		replies := []ds.MessageEmbed{
+			{Title: "Mutli-page Embed Example", Description: "First Page", Fields: []*ds.MessageEmbedField{{Name: "Temperature", Value: "16C"},{Name: "Article", Value: "India reports highest daily coronavirus deaths"}, {Name: "Other", Value: "9382774"}}, Footer: &ds.MessageEmbedFooter{Text: "Data provided by datasource"}},
+			{Title: "Mutli-page Embed Example", Description: "Second Page", Fields: []*ds.MessageEmbedField{{Name: "Temperature", Value: "25C"},{Name: "Article", Value: "Night-time splashdown for four ISS astronauts"}, {Name: "Other", Value: "32905934"}}, Footer: &ds.MessageEmbedFooter{Text: "Data provided by datasource"}},
+			{Title: "Mutli-page Embed Example", Description: "Third Page", Fields: []*ds.MessageEmbedField{{Name: "Temperature", Value: "-12C"},{Name: "Article", Value: "The dogs that stayed in Chernobyl"}, {Name: "Other", Value: "0340043"}}, Footer: &ds.MessageEmbedFooter{Text: "Data provided by datasource"}},
+		}
+		message, _ := s.ChannelMessageSendEmbed(m.ChannelID, &replies[0])
+		go spinReaction(message.ID, m.ChannelID, replies, s)
 	} else {				// Result
 		s.ChannelMessageSendEmbed(m.ChannelID, &reply)
 	}
+}
+
+func spinReaction(messageID string, channelID string, replies []ds.MessageEmbed, s *ds.Session,){
+	// Add reactions
+	s.MessageReactionAdd(channelID, messageID, "◀")
+	s.MessageReactionAdd(channelID, messageID, "▶")
+	s.MessageReactionAdd(channelID, messageID, "❌")
+
+	var index = 0
+	for i := 0; i < 30;{
+		/* Timeout Counter
+		go func(i *int){
+			for *i < 30 {
+
+			}
+		}(&i)
+		 */
+
+		// Gets newest message
+		message, _ := s.ChannelMessage(channelID, messageID)
+		if users, _ := s.MessageReactions(channelID, messageID, "◀", 2, "", ""); len(users) > 1{ // "◀"
+			index -= 1
+
+			s.ChannelMessageEditEmbed(channelID, message.ID, &replies[index])
+			s.MessageReactionRemove(channelID, messageID, "◀", users[0].ID)
+
+			// Reset counter
+			i = 0
+		} else if users, _ := s.MessageReactions(channelID, messageID, "▶", 2, "", ""); len(users) > 1{ // "▶"
+			index += 1
+
+			s.ChannelMessageEditEmbed(channelID, message.ID, &replies[index])
+			s.MessageReactionRemove(channelID, messageID, "▶", users[0].ID)
+
+			// Reset counter
+			i = 0
+		} else if users, _ := s.MessageReactions(channelID, messageID, "❌", 2, "", ""); len(users) > 1{ // "❌"
+			s.MessageReactionsRemoveAll(channelID, messageID)
+			break
+		}
+
+		time.Sleep(1 * time.Second)
+		i += 1
+	}
+
+	// Done waiting, remove all reactions
+	s.MessageReactionsRemoveAll(channelID, messageID)
 }
 
 func parseContent(content string)(string, string, string, map[string]string, error){
