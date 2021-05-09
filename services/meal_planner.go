e6fa24393ea33bfae45109c411ec5cefaed9f6d4
Sabana Ghale <thomasvl@stud.ntnu.no>
2021-05-09 18:06:22 +0100

    Updated discord message formating and added new decoder

--- services/meal_planner.go
+++ services/meal_planner.go
@@ -3,8 +3,10 @@ package services
 import (
 	dataRequests "assistant/DataRequests"
 	"assistant/utils"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -18,16 +20,17 @@ func HandleRouteToMeals(subRoute string, flags map[string]string) (discordgo.Mes
 			return mealEmbed, nil
 		} else { //Get from fridge
 			recipes, err := getRecipeFromFridge()
-			fmt.Println(len(recipes))
 			if err != nil {
 				return mealEmbed, err
 			}
-			fmt.Println(len(recipes))
-
 			mealEmbed.Title = recipes[0].Name
-			mealEmbed.Footer.IconURL = recipes[0].Image
-			mealEmbed.Description = "nice need to parse missing ingredients, hope the picture worked"
-			fmt.Println("we made it here bro")
+
+			mealEmbed.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: recipes[0].Image}
+			description := "Missed ingredients:"
+			for _, ingredients := range recipes[0].MissedIngredients {
+				description += ingredients.IngredientName + " "
+			}
+			mealEmbed.Description = description
 
 			return mealEmbed, nil
 		}
@@ -48,12 +51,12 @@ func HandleRouteToMeals(subRoute string, flags map[string]string) (discordgo.Mes
 	}
 }
 
-func getRecipeFromFridge() ([]utils.Recipe, error) {
+func getRecipeFromFridge() (utils.Recipe, error) {
 	//Use a test fridge until we have an implementation of UserData
 	fridge := createTestFridge()
 	//Check if fridge is empty
 	if len(fridge.Ingredients) == 0 {
-		return []utils.Recipe{}, errors.New("fridge is empty")
+		return utils.Recipe{}, errors.New("fridge is empty")
 	}
 	//Fridge is not empty
 	var ingredientString string
@@ -61,14 +64,14 @@ func getRecipeFromFridge() ([]utils.Recipe, error) {
 		ingredientString += ingredient + ","
 	}
 	//Create url and recipe struct for holding data
-	url := "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + "milk" + "&number=2&apiKey=c7939a239ecd43c49c1654aff9d387d6"
-	var recipe []utils.Recipe
+	url := "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + "milk" + "&number=1&apiKey=c7939a239ecd43c49c1654aff9d387d6"
+	var recipe utils.Recipe
 	//Use GetAndDecode function to decode it into recipe struct
 	err := dataRequests.GetAndDecodeURL(url, recipe)
+
 	//Check if there was any errors in fetching and decoding the url
 	if err != nil {
-		fmt.Println("NO")
-		return []utils.Recipe{}, err
+		return utils.Recipe{}, err
 	}
 	return recipe, nil
 }
@@ -78,4 +81,16 @@ func createTestFridge() utils.Fridge {
 	var fridge utils.Fridge
 	fridge.Ingredients = append(fridge.Ingredients, "Apple", "Milk", "Chicken", "Butter")
 	return fridge
+}
+
+func decodemystuff() (utils.Recipe, error) {
+	url := "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + "milk" + "&number=1&apiKey=c7939a239ecd43c49c1654aff9d387d6"
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Oh no")
+	}
+	var recipe utils.Recipe
+	json.NewDecoder(response.Body).Decode(&recipe)
+	fmt.Println(recipe[0], response.StatusCode)
+	return recipe, nil
 }
