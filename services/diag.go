2611cc3067c5c0fe27b31b13270ed5a3b85820be
Håvard Fjørkenstad <haavarfj@stud.ntnu.no>
2021-05-15 21:51:48 +0000

    Update diag.go

--- services/diag.go
+++ services/diag.go
@@ -1,33 +1,25 @@
-package services
-
-import (
-	"assistant/utils"
-	"errors"
-	"github.com/bwmarrin/discordgo"
-)
-
 func HandleRouteToDiag(subRoute string, flags map[string]string)(discordgo.MessageEmbed, error){
 	var diagEmbed = discordgo.MessageEmbed{}
 	switch subRoute{
-	case utils.View:
-		if len(flags) != 0{
-			return diagEmbed, nil
-		} else {
-			return diagEmbed, nil
+	case utils.View, utils.Get, utils.Check:
+		diagEmbed.Title = "Diagnostics"
+		diagEmbed.Fields = []*discordgo.MessageEmbedField{
+			{
+				Name: "NewsAPI",
+				Value: getStatusCode("___"),
+			},
+			{Name: "OpenWeatherMap", Value: getStatusCode("http://pro.openweathermap.org/data/2.5/forecast/daily?q=gj%C3%B8vik&cnt=3&appid=94aad1fbb7ae86f5de4cf9aafc51e2e2")},
+			{Name: "Big News Search API", Value: "___"},
+			{Name: "Spoonacular", Value: getStatusCode("https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&apiKey=" + utils.MealKey)},
+
 		}
-	case utils.Set:
-		if len(flags) != 0{
-			return diagEmbed, nil
-		} else {
-			return diagEmbed, errors.New("flags are needed")
-		}
-	case utils.Remove:
-		if len(flags) != 0{
-			return diagEmbed, nil
-		} else {
-			return diagEmbed, errors.New("flags are needed")
-		}
+		return diagEmbed, nil
 	default:
 		return diagEmbed, errors.New("sub route not recognized")
 	}
+}
+
+func getStatusCode(url string)string{
+	resp, _ := http.Get(url)
+	return resp.Status
 }
