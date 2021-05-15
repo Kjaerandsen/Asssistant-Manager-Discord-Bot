9214a80b1adfffffedcfc6e69764282a599032f8
ThomasMACHINE <53544690+ThomasMACHINE@users.noreply.github.com>
2021-05-15 23:08:54 +0100

    fixed package error

--- services/diag.go
+++ services/diag.go
@@ -1,13 +1,21 @@
-func HandleRouteToDiag(subRoute string, flags map[string]string)(discordgo.MessageEmbed, error){
+package services
+
+import (
+	"assistant/utils"
+	"errors"
+	"github.com/bwmarrin/discordgo"
+	"net/http"
+)
+
+func HandleRouteToDiag(subRoute string, flags map[string]string) (discordgo.MessageEmbed, error) {
 	var diagEmbed = discordgo.MessageEmbed{}
-	switch subRoute{
+	switch subRoute {
 	case utils.View, utils.Get, utils.Check:
 		diagEmbed.Title = "Diagnostics"
 		diagEmbed.Fields = []*discordgo.MessageEmbedField{
 			{Name: "OpenWeatherMap", Value: getStatusCode("http://pro.openweathermap.org/data/2.5/forecast/daily?q=gj%C3%B8vik&cnt=3&appid=94aad1fbb7ae86f5de4cf9aafc51e2e2")},
 			{Name: "Bing News Search API", Value: getStatusCode("https://api.bing.microsoft.com/v7.0/news/search")},
 			{Name: "Spoonacular", Value: getStatusCode("https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&apiKey=" + utils.MealKey)},
-
 		}
 		return diagEmbed, nil
 	default:
@@ -15,7 +23,7 @@ func HandleRouteToDiag(subRoute string, flags map[string]string)(discordgo.Messa
 	}
 }
 
-func getStatusCode(url string)string{
+func getStatusCode(url string) string {
 	resp, _ := http.Get(url)
 	return resp.Status
 }
